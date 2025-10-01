3fab6682270fe3c81373be160d70e118b80ab512
antax02 <anton.axelsson@nyckelpiga.net>
2025-10-01 19:15:35 +0200

    Save settings on slider drag end and menu exit
    Settings are now saved when volume sliders' drag ends and when exiting the settings menu via back button or cancel input. This prevents excessive saves during slider movement and ensures changes are persisted when leaving the menu.

--- scripts/ui/settings_menu.gd
+++ scripts/ui/settings_menu.gd
@@ -1,13 +1,10 @@
 extends Control
 
-
 @onready var master_volume_slider: HSlider = $Panel/MarginContainer/VBoxContainer/MasterVolumeContainer/MasterVolumeSlider
 @onready var music_volume_slider: HSlider = $Panel/MarginContainer/VBoxContainer/MusicVolumeContainer/MusicVolumeSlider
 @onready var sfx_volume_slider: HSlider = $Panel/MarginContainer/VBoxContainer/SFXVolumeContainer/SFXVolumeSlider
-
 @onready var fullscreen_check_box: CheckBox = $Panel/MarginContainer/VBoxContainer/FullscreenCheckBox
 @onready var screenshake_check_box: CheckBox = $Panel/MarginContainer/VBoxContainer/screenshakeCheckBox
-
 @onready var back_button: Button = $Panel/MarginContainer/VBoxContainer/Back
 
 func _ready():
@@ -20,6 +17,11 @@ func _ready():
 	master_volume_slider.value_changed.connect(_on_master_changed)
 	music_volume_slider.value_changed.connect(_on_music_changed)
 	sfx_volume_slider.value_changed.connect(_on_sfx_changed)
+	
+	master_volume_slider.drag_ended.connect(_on_slider_drag_ended)
+	music_volume_slider.drag_ended.connect(_on_slider_drag_ended)
+	sfx_volume_slider.drag_ended.connect(_on_slider_drag_ended)
+	
 	fullscreen_check_box.toggled.connect(_on_fullscreen_toggled)
 	screenshake_check_box.toggled.connect(_on_screenshake_toggled)
 	
@@ -29,16 +31,16 @@ func _ready():
 func _on_master_changed(value):
 	SettingsManager.master_volume = value
 	SettingsManager.apply_settings()
-	SettingsManager.save_settings()
 
 func _on_music_changed(value):
 	SettingsManager.music_volume = value
 	SettingsManager.apply_settings()
-	SettingsManager.save_settings()
 
 func _on_sfx_changed(value):
 	SettingsManager.sfx_volume = value
 	SettingsManager.apply_settings()
+
+func _on_slider_drag_ended(_value_changed: bool):
 	SettingsManager.save_settings()
 
 func _on_fullscreen_toggled(pressed):
@@ -52,7 +54,9 @@ func _on_screenshake_toggled(pressed):
 	
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
+		SettingsManager.save_settings()
 		SceneTransition.change_scene("res://scenes/ui/main_menu.tscn")
 
 func _on_back_pressed():
+	SettingsManager.save_settings()
 	SceneTransition.change_scene("res://scenes/ui/main_menu.tscn")
